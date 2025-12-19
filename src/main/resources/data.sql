dd7321aca3bc30bbe7c87aee988739afc525d026
Asel Williams <aselwilliams22@gmail.com>
2025-12-19 00:14:32 -0600

    implement vacancy CRUD support, adds vacancy dao, service and integrate endpoints for applicants and employers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     password VARCHAR(50)
-);
+    );
 
 INSERT INTO users (name, password)
 VALUES ('John Doe', '123456'),('Jane Smith', '123456aaa');
